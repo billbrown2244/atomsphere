875747de8f0802ae99368a2ec6382e88b5dda185
Bill Brown <billbrown@colorfulsoftware.com>
2006-05-30 18:04:39 +0000

    [atomsphere] - removed some unneeded import declarations.

--- src/main/java/com/colorful/atom/beans/Author.java
+++ src/main/java/com/colorful/atom/beans/Author.java
@@ -18,7 +18,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package com.colorful.atom.beans;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class Author {
