2c57e42d09dbd591b25a046f6fdab0e949eda41e
Bill Brown <billbrown@colorfulsoftware.com>
2009-11-26 03:00:21 +0000

    [atomsphere] - updating license headers.

--- src/main/java/com/colorfulsoftware/atom/AtomTextConstruct.java
+++ src/main/java/com/colorfulsoftware/atom/AtomTextConstruct.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2009 William R. Brown <wbrown@colorfulsoftware.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /* Change History:
  * 2008-03-16 wbrown - made class immutable.
@@ -264,8 +263,8 @@ class AtomTextConstruct implements Serializable {
 	}
 
 	/**
-	 * Shows the contents of the <content>, <rights>, <subtitle>, <summary> or
-	 * <title> elements.
+	 * Shows the contents of the &lt;content>, &lt;rights>, &lt;subtitle>,
+	 * &lt;summary> or &lt;title> elements.
 	 */
 	@Override
 	public String toString() {
